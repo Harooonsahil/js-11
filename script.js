20d25a30457aa55d0c19ead5f457aaaf8cab9177
Harooonsahil <haroonsahil671@gmail.com>
2024-07-16 02:36:39 +0500

    given assignment

---
+++ script.js
@@ -0,0 +1,23 @@
+  // EXERCISE 1
+ /*let str = "Welcome to javascript";
+console.log(str.length);*/
+
+// EXERCISE 2
+ /*let str ="programming is awesome";
+let b=str.slice(15);
+console.log(b);*/
+
+//EXERRCISE 3
+/*let str ="Hello, universe!";
+  console.log(str.slice(0,5)); */
+
+  // EXERCISE 4
+ /* let str = "Full stack developer";
+  console.log(str.slice(5,10));*/
+
+  // EXERCISE 5
+ /* let str ="javascript strings";
+  console.log(str.slice(14));*/
+  
+
+
